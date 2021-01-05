ee985072a1017d57db0b6a99883d28546ea427a7
Mike Guijarro <a01612042@itesm.mx>
2021-01-04 22:33:10 -0600

    Connection of Cloudinary service

--- src/models/CampaignImage.js
+++ src/models/CampaignImage.js
@@ -3,6 +3,7 @@ const { model, Schema } = require('mongoose');
 const CampaignImage = new Schema({
     title: String,
     description: String,
+    campaign: String,
     imageURL: String,
     //Public Id provided by claudinary service
     public_id: String,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { route } = require("../app");
 const router = express.Router();
+
+const CampaignImage = require("../models/CampaignImage.js");
+const cloudinary = require("cloudinary");
+const fs = require("fs-extra"); 
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+  
 
 //Main routes of the app
 router.get("/", (req, res) => {
@@ -34,6 +46,29 @@ router.get("/gallery", (req, res) => {
 
 router.get("/gallery/add", (req, res) => {
     res.render("gallery_form", {galleryForm: true});
+});
+
+router.post("/gallery/add", async (req, res)=> {
+    //console.log(req.body);
+    //console.log(req.file);
+
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+    //console.log(result);
+
+    const {title, campaign, description} = req.body;
+    const newImageGallery = new CampaignImage({
+        title: title,
+        description: description,
+        campaign: campaign,
+        imageURL: result.url,
+        public_id: result.public_id,
+    });
+
+    await newImageGallery.save();
+    
+    await fs.unlink(req.file.path);
+
+    res.send("Hi!")
 });
 
 router.get("/faq", (req, res)=> {
