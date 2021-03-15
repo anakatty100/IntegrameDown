5d793a18d0923cb6b7ccf2944b8a6ab3da6dfbe7
Mike Guijarro <a01612042@itesm.mx>
2021-03-14 22:23:02 -0500

    Form validation implemented

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,67 @@
 import EditorJS from '@editorjs/editorjs';
+import ImageTool from '@editorjs/image';
 
-const editor = new EditorJS();
+const saveBtn = document.querySelector("#save-event-btn");
+
+// Editor Js
+const editor = new EditorJS({
+    tools: {
+        image: {
+            class: ImageTool,
+            config: {
+                endpoints: {
+                    byFile: '/admin/event/body-image-file', // Your backend file uploader endpoint
+                    byUrl: '/admin/body-image-url', // Your endpoint that provides uploading by Url
+                }
+            }
+        }
+    },
+    onReady: () => {
+        //Activate button for saving
+        saveBtn.addEventListener("click", saveEditorData);
+    }
+});
+
+const saveEditorData = async (e) => {
+    try {
+        const outputData = await editor.save();
+        console.log(outputData);
+    } catch (e) {
+        console.error("Saving failed", e);
+    }
+
+}
+
+const formEvent = document.querySelector("#form-event");
+const inputs = formEvent.querySelectorAll("input");
+const isValidInputs = [];
+
+const inputChangeHandler = (e) => {
+    if (e.target.value) {
+        for(let i = 0; i < inputs.length;i++) {
+            if (inputs[i] === e.target) {
+                isValidInputs[i] = true;
+                break;
+            }
+        }
+        validateInputHandler();
+    }
+};
+
+const validateInputHandler = () => {
+    let allFormIsValid = true;
+    for (let i = 0; i < isValidInputs.length; i++) {
+        if (isValidInputs[i] === false) {
+            allFormIsValid = false;
+            break;
+        }
+    }
+    console.log(isValidInputs);
+    console.log("AllFormIsValid: ",allFormIsValid);
+    saveBtn.disabled = !allFormIsValid;
+};
+
+inputs.forEach((input, i) => {
+    input.addEventListener("input", inputChangeHandler);
+    isValidInputs[i] = false;
+});
--- src/routes/admin/event.js
+++ src/routes/admin/event.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const cloudinaryHandler = require("../../lib/cloudinary");
 
 /* router.get("/events", (req, res) => {
     res.render("events", { events: true });
@@ -12,6 +13,29 @@ router.get("/events/:id", (req, res) => {
 
 router.get("/event", (req, res) => {
     res.render("admin/events/event_form", { event: true });
+});
+
+router.post("/event/body-image-file", async (req, res) => {
+    try {
+        const result = await cloudinaryHandler.upload(req.file.path);
+
+        // const newImage = new Image({
+        //     title: "Content Generic Event Image",
+        //     imageURL: result.url,
+        //     public_id: result.public_id,
+        // });
+
+        res.json({
+            "success": 1,
+            "file": {
+                "url": result.url,
+                "public_id": result.public_id
+            }
+        });
+        
+    } catch (e) {
+        console.error(e);
+    }
 });
 
 module.exports = router;
